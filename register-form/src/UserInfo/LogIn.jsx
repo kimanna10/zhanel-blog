33e635e673c2354b3f2a3c8172f527a1dadef84d
Anna Kim <55574696+kimanna10@users.noreply.github.com>
2025-06-24 00:44:11 +0500

    change api to production

--- register-form/src/UserInfo/LogIn.jsx
+++ register-form/src/UserInfo/LogIn.jsx
@@ -22,7 +22,7 @@ export default function Login() {
     setError("");
 
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://zhanel-blog.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
